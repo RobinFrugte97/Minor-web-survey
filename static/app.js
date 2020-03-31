2bbbbcf3ea37adc8f599681dba7c2d9783260e3f
RobinFrugte97 <robin.frugte@hotmail.com>
2020-03-31 12:42:14 +0200

    Fixed slider code, added feedback to text input

--- static/app.js
+++ static/app.js
@@ -5,34 +5,49 @@ slider.min = 1
 slider.max = 10
 slider.step = 1
 slider.setAttribute('aria-hidden', 'true')
+var test = []
 
 var sliderContainer = document.getElementById('nummer-container')
+if (sliderContainer !== null) {
+    sliderContainer.appendChild(slider) 
+}
 
-sliderContainer.appendChild(slider)
-if (typeof Array.prototype.forEach == 'function') {
+if (document.addEventListener) {
     slider.addEventListener('input', function () {
         nummer.value = slider.value
     })
-    nummer.addEventListener('input', function () {
-        slider.value = nummer.value
+    // Event 'change' voor IE.
+    slider.addEventListener('change', function () {
+        nummer.value = slider.value
     })
-} else {
-    if (document.addEventListener) {
-        slider.addEventListener('change', function () {
-            nummer.value = slider.value
-        })
+    if (nummer !== null) {
         nummer.addEventListener('input', function () {
             slider.value = nummer.value
         })
-    } else {
-        slider.attachEvent("oninput", function () {
-            nummer.value = slider.value
-
-        });
+    }
+} else {
+    slider.attachEvent("oninput", function () {
+        nummer.value = slider.value
+    })
+    // Event 'onchange' voor IE.
+    slider.attachEvent("onchange", function () {
+        nummer.value = slider.value
+    })
+    if (nummer !== null) {
         nummer.attachEvent("oninput", function () {
             slider.value = nummer.value
-
         })
-    } 
-}
+    }
+} 
 
+var studentnummerLabel = document.getElementById('studentnummerLabel')
+var studentnummerInput = document.getElementById('studentnummerInput')
+
+studentnummerInput.addEventListener('keydown', function() {
+    var chars = studentnummerInput.value.length + 1
+    if (chars < 9) {
+        studentnummerLabel.innerHTML = 'Nog '+ (9 - (studentnummerInput.value.length + 1)) + ' characters..'
+    } else if (chars >= 9){
+        studentnummerLabel.innerHTML = ''
+    }
+})
